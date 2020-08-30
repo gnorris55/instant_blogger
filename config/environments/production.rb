cc66fa359799b38badea5895bc4405277856d467
Graeme Norris <gnorris@shawnigan.ca>
2020-08-30 14:24:47 -0700

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => "instant-blogger99.herokuapp.com" }
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: "odin-cats.com",
     authentication: "plain",

a37316d9fa9db286f9bde4e983542bf1e1902a33
Graeme Norris <gnorris@shawnigan.ca>
2020-08-30 12:08:34 -0700

    configured the database to be postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_29_221254) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.integer "author_id"
     t.string "title"

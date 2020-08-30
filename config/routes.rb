5b41dbb9666eecaaa7a3baa6afe1ba3e1e45dd67
Graeme Norris <gnorris@shawnigan.ca>
2020-08-30 12:43:36 -0700

    added a root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'blogs/home'
   resources :blogs
   resources :users
+  root 'blogs#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
